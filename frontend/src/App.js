66cd3ae8ff65de679481050346fea2d7762b4b1b
prashasti-07 <prashastigarg.btele20@pec.edu.in>
2023-08-18 14:44:11 +0530

    added abi.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,18 @@ import Header from './Components/Header/Header';
 import BuyerDashboard from './Components/buttons'; // Import the BlueBoxWithButtons component
 import SellerDashboard from './pages/sellerDashboard';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ethers } from 'ethers'
+import contractABI from './abi.json'
 
+const contractAddress = '0x33C35Df6B92FC699e0d0892363Fb1fBB93ca5FEc'
+const loadData = async () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
 
+  const contract = new ethers.Contract(contractAddress, contractABI, provider);
+  const greeting = await contract.transferOwnership();
+  alert(greeting);
+
+}
 function App() {
   return (
     <BrowserRouter>
@@ -19,3 +29,4 @@ function App() {
 }
 
 export default App;
+
