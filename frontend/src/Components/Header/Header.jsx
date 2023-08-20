a92854fbb3c591336bd190b78f30f87358354dd1
Sarijan <sarijansharma15march2001@gmail.com>
2023-08-21 02:36:06 +0530

    added tokenomics file

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,113 +1,4 @@
-// import { useState } from 'react';
-
-// import { AppBar, Toolbar, Box, Typography, IconButton, Drawer, List, styled } from '@mui/material';
-// import { Menu } from '@mui/icons-material';
-
-// import { Link } from 'react-router-dom';
-// import { Box, Button } from '@mui/material';
-// //components
-// import CustomButtons from './CustomButtons';
-// import Search from './Search';
-
-// const StyledHeader = styled(AppBar)`
-//     background: #2874f0;
-//     height: 55px;
-// `;
-
-// const Component = styled(Link)`
-//     margin-left: 12%;
-//     line-height: 0;
-//     color: #FFFFFF;
-//     text-decoration: none;
-// `;
-
-// const SubHeading = styled(Typography)`
-//     font-size: 10px;
-//     font-style: italic;
-// `
-
-// const PlusImage = styled('img')({
-//     width: 10,
-//     height: 10,
-//     marginLeft: 4
-// })
-
-// const MenuButton = styled(IconButton)(({ theme }) => ({
-//     display: 'none',
-//     [theme.breakpoints.down('sm')]: {
-//         display: 'block'
-//     }
-// }));
-
-// const CustomButtonWrapper = styled('span')(({ theme }) => ({ 
-//     margin: '0 5% 0 auto', 
-//     [theme.breakpoints.down('sm')]: {
-//         display: 'none'
-//     }
-// }));
-
-// const Header = () => {
-//     const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
-//     const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
-
-//     const [open, setOpen] = useState(false);
-
-//     const handleClose = () => {
-//         setOpen(false);
-//     }
-
-//     const handleOpen = () => {
-//         setOpen(true);
-//     }
-
-//     const list = () => (
-//         <Box style={{ width: 250 }} onClick={handleClose}>
-//             <List>
-//                 <listItem button>
-//                     <CustomButtons />
-//                 </listItem>
-//             </List>
-//         </Box>
-//     );
-
-
-//     return (
-//         <StyledHeader position="fixed">
-//             <Toolbar style={{ minHeight: 55 }}>
-//                 <MenuButton
-//                     color="inherit"
-//                     onClick={handleOpen}
-//                 >
-//                     <Menu />
-//                 </MenuButton>
-
-//                 <Drawer open={open} onClose={handleClose}>
-//                     {list()}
-//                 </Drawer>
-
-//                 <Component to='/'>
-//                     <img src={logoURL} style={{ width: 75 }} />
-//                     <Box component="span" style={{ display: 'flex' }}>
-//                         <SubHeading>Explore&nbsp;
-//                             <Box component="span" style={{color:'#FFE500'}}>
-//                                 Plus
-//                             </Box>
-//                         </SubHeading>
-//                         <PlusImage src={subURL} />
-//                     </Box>
-//                 </Component>
-//                 <Search />
-//                 <CustomButtonWrapper>
-//                     <CustomButtons />
-//                 </CustomButtonWrapper>
-//             </Toolbar>
-//         </StyledHeader>
-//     )
-// }
-
-// export default Header;
-
-import React, { useState } from 'react'; // Don't forget to import React
+import React, { useState } from 'react'; 
 import { AppBar, Toolbar, Box, Typography, IconButton, Drawer, List, styled, ButtonGroup } from '@mui/material';
 import { Menu } from '@mui/icons-material';
 import { ListItem } from '@mui/material';
@@ -182,6 +73,25 @@ const Header = ({ web3, connectWeb3 }) => {
             </List>
         </Box>
     );
+    
+        const handleDownloadClick = () => {
+         
+          const pdfUrl = process.env.PUBLIC_URL + '/path/to/your/pdf/tokenomics.pdf';
+          
+        
+          const link = document.createElement('a');
+          link.href = pdfUrl;
+          link.target = '_blank'; 
+          link.download = 'tokenomics.pdf'; 
+          
+          
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link); 
+      
+          
+        };
+    
 
     return (
         <StyledHeader position="fixed">
@@ -208,7 +118,9 @@ const Header = ({ web3, connectWeb3 }) => {
                 </Component>
                 <ButtonGroup style={{ margin: "2px 4px 2px 9rem", display: "flex", justifyContent: "space-between", minWidth: "500px" }}>
                     <Button onClick={connectWeb3} disabled={web3 ? true : false} variant='contained'>{web3 ? "Wallet Connected" : "Connect Wallet"}</Button>
-                    <Button variant='contained'>Download Tokenomics</Button>
+                    <Button variant="contained" onClick={handleDownloadClick}>
+                            Download Tokenomics
+                        </Button>
                 </ButtonGroup>
                 <CustomButtonWrapper>
                     <CustomButtons />
--- frontend/src/pages/sellerDashboard.jsx
+++ frontend/src/pages/sellerDashboard.jsx
@@ -1,87 +1,3 @@
-// import React from 'react';
-// import { Box, Paper, Typography, Button } from '@mui/material';
-// import './sellerDash.css';
-
-// function SellerDashboard() {
-//   const walletBalance = 5000; // Example wallet balance
-//   const transactionHistory = [
-//     { id: 1, description: 'coupon sold', amount: +100 },
-//     { id: 2, description: 'paid for loyalty', amount: -20 },
-//     { id: 3, description: 'Refund', amount: +100 },
-//     { id: 4, description: 'xyz', amount: 75 },
-//     { id: 5, description: 'zzzz', amount: -150 },
-//     { id: 6, description: 'Recieved', amount: 200 },
-//     { id: 7, description: 'Flipcoin', amount: -50 },
-//     // ... more transactions
-//   ];
-
-
-
-//   const renderTransactionHistory = () => {
-//     // Render last 5 transaction history items
-//     const last5Transactions = transactionHistory.slice(-5); 
-//     return last5Transactions.map((transaction) => (
-//       <Paper key={transaction.id} elevation={3} className="transaction-box">
-//         <Typography>{transaction.description}</Typography>
-//         <Typography>{transaction.amount} USD</Typography>
-//       </Paper>
-//     ));
-//   };
-
-//   const renderCoupons = () => {
-//     // Replace with actual data
-//     const coupons = [
-//       { id: 1, title: 'Coupon 1', description: 'Description of Coupon 1' },
-//       { id: 2, title: 'Coupon 2', description: 'Description of Coupon 2' },
-//       { id: 3, title: 'Coupon 3', description: 'Description of Coupon 3' },
-//       { id: 4, title: 'Coupon 4', description: 'Description of Coupon 4' },
-//       { id: 5, title: 'Coupon 5', description: 'Description of Coupon 5' },
-//     ];
-
-//     return coupons.map((coupon) => (
-//       <Paper key={coupon.id} elevation={3} className="coupon-box">
-//         <Typography variant="h6">{coupon.title}</Typography>
-//         <Typography variant="body2">{coupon.description}</Typography>
-//         {/* Add any additional coupon details */}
-//       </Paper>
-//     ));
-//   };
-
-//   return (
-//     <div className="seller-dashboard">
-//       <div className="left-section">
-//         <Paper elevation={3} className="left-box">
-//           <Typography variant="h6">Wallet Balance: {walletBalance} USD</Typography>
-//           {/* Balance bar */}
-//           <div className="balance-bar" style={{ width: (walletBalance / 50000) * 100 + '%' }}></div>
-//           <Typography variant="body2">Last 5 Transactions:</Typography>
-//           <Box mt={2}>
-//             {renderTransactionHistory()}
-//           </Box>
-//           <Box mt={2} textAlign="center">
-//             <Button variant="contained">Load More</Button>
-//           </Box>
-//         </Paper>
-//       </div>
-//       <div className="right-section">
-//         <Box>
-//           <Typography variant="h5">Coupons Being Offered</Typography>
-//           <Box mt={2} display="flex" justifyContent="space-between">
-//             {renderCoupons()}
-//           </Box>
-//         </Box>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default SellerDashboard;
-
-
-
-
-
-
 import React, { useState, useEffect } from 'react';
 import { Box, Paper, Typography, Button, CircularProgress, Backdrop, FormControl, InputLabel, MenuItem, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from '@mui/material';
 import './sellerDash.css';
@@ -241,7 +157,7 @@ function SellerDashboard({ web3 }) {
               style={{ paddingBottom: "20px" }}
             />
             {isRedeeming ? (
-              <CircularProgress size={24} /> // Show loading icon if redeeming
+              <CircularProgress size={24} /> 
             ) : (
               <Button variant="contained" color="primary" onClick={handleOfferTokens}>Submit</Button>
             )}
